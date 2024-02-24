449f2b000f9f93547e26d58fc52fd09812a0b25d
schaaya <schaaya@umich.edu>
2024-02-24 15:29:28 -0500

    Change header tag for concise length

--- sappa-portfolio/src/components/Contact/Contact.jsx
+++ sappa-portfolio/src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact Me</h2>
+        <h3>Contact Me</h3>
         
       </div>
       <ul className={styles.links}>
